8ed70eb6b8c5af1388192486babf7c66eed8e271
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-11-25 07:19:54 +0100

    Agregar enpoints, modelos, controlador de inscripciones

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -6,5 +6,6 @@ router.use("/alumnos", require("./api/apiAlumnosRoutes"));
 router.use("/materias", require("./api/apiMateriasRoutes"));
 router.use("/login", require("./api/apiLoginRoutes"));
 router.use("/opiniones", require("./api/apiOpinionesRoutes"));
+router.use("/inscripciones", require("./api/apiInscripcionesRoutes"));
 
 module.exports = router;
